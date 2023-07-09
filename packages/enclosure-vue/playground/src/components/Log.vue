6c22431bd07e95b3529ce831f837a8ac380f21da
mewton <capella@mail.mewton.jp>
2023-07-10 00:52:23 +0900

    fix: update playground layout

---
+++ packages/enclosure-vue/playground/src/components/Log.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Log {
+  id: number
+  time: string
+  event: string
+  key: string
+  value: string
+}
+
+const logs = ref<Log[]>([])
+
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Time</th>
+        <th>Event</th>
+        <th>Key</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="log in logs" :key="log.id">
+        <td>{{ log.time }}</td>
+        <td>{{ log.event }}</td>
+        <td>{{ log.key }}</td>
+        <td>{{ log.value }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
--- packages/enclosure-vue/playground/src/views/HomeView.vue
+++ packages/enclosure-vue/playground/src/views/HomeView.vue
@@ -4,23 +4,18 @@ import {
   type ChildToParent
 } from '../../../src/components/types'
 
-const parentToChild: ParentToChild = (location) => { return location.hash }
+const parentToChild: ParentToChild = (location) => {
+  return location.hash
+}
 
-const childToParent: ChildToParent = (url) => { return '/hoge' }
+const childToParent: ChildToParent = (url) => {
+  return '/hoge'
+}
 </script>
 
 <template>
-  <main>
-    <BridgeFrame
-      initial-src="http://localhost:8081/"
-      :parent-to-child="parentToChild"
-      :child-to-parent="childToParent" />
-  </main>
+  <BridgeFrame
+    initial-src="http://localhost:8081/"
+    :parent-to-child="parentToChild"
+    :child-to-parent="childToParent" />
 </template>
-
-<style scoped>
-main {
-  height: 80vh;
-  width: 45vh;
-}
-</style>
